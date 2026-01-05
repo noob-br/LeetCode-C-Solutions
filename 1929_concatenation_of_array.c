e23c99cb7aa0cb6246511f0818e36e26f4d0e133
noob-br <marciors05@hotmail.com>
2026-01-05 10:05:54 -0300

    Create 1929_concatenation_of_array.c
    Add solution for 1929

---
+++ 1929_concatenation_of_array.c
@@ -0,0 +1,18 @@
+/**
+ * Problem: 1929. Concatenation of Array
+ * Link: https://leetcode.com/problems/concatenation-of-array/
+ * Method: Manual memory allocation (malloc) for maximum integrity. 🛡️
+ */
+    
+    int n = numsSize;
+    *returnSize = 2 * n;
+
+    int* ans = malloc(2 * n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        ans[i] = nums[i]; 
+        ans[i + n] = nums[i];
+
+    }
+    return ans;
+}
