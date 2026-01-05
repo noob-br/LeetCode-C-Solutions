47367e69da4dce7db64523e6eee40a5f93c10640
noob-br <marciors05@hotmail.com>
2026-01-05 10:07:37 -0300

    Update 2235_add_two_integers.c

--- 2235_add_two_integers.c
+++ 2235_add_two_integers.c
@@ -1,3 +1,9 @@
+/**
+ * Problem: 2235. Add Two Integers
+ * Link: https://leetcode.com/problems/add-two-integers/
+ * Method: Direct addition. The simplest gate to LeetCode. 🛡️
+ */
+
 int sum(int num1, int num2) {
     return num1 + num2;
 }
